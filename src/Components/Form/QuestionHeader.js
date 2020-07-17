214e41e3c4fef35395fc71ecaa04fb2e6c9deb7b
Rohit Saini <sidsaini1196@gmail.com>
2020-07-17 18:55:48 +0530

    added dynamic question add

--- src/Components/Form/QuestionHeader.js
+++ src/Components/Form/QuestionHeader.js
@@ -1,18 +1,105 @@
 import React from 'react'
-import { Paper } from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import TextField from '@material-ui/core/TextField';
+
+
 
 function QuestionsHeader() {
+
+  const [questions, setQuestions]= React.useState([{questionText: "Question", open: false}]);
+
+  function addMoreQuestionField(){
+      expandCloseAll(); //I AM GOD
+      setQuestions(questions=> [...questions, {questionText: "Question", open: true}]);
+  }
+
+  function expandCloseAll(){
+    let qs = [...questions]; 
+     for (let j = 0; j < qs.length; j++) {  
+      qs[j].open = false;
+     }
+     setQuestions(qs);
+  }
+
+  function handleExpand(i){
+    let qs = [...questions]; 
+    for (let j = 0; j < qs.length; j++) {
+      if(i ==j ){
+        qs[j].open = true;
+      } else{
+        qs[j].open = false;
+       }
+    }
+     setQuestions(qs);
+  }
+
+  function questionsUI(){
+    return  questions.map((ques, i)=> (
+      <div key={i}>
+          <div style={{marginBottom: "9px"}}>
+          {/* onChange={(e)=>{handleInputAddMember(e.target.value, i)}} */}
+            <ExpansionPanel onChange={()=>{handleExpand(i)}} expanded={questions[i].open}>
+              <ExpansionPanelSummary
+              
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                elevation={1} style={{width:'100%'}}
+              >
+                { !questions[i].open ? (
+              <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginLeft: '0px', paddingTop: '20px', paddingBottom: '20px'}}>
+                {/* <TextField id="standard-basic" label=" " value="Question" InputProps={{ disableUnderline: true }} />  */}
+                <Typography variant="subtitle1">Form description {i+1}</Typography>
+                
+                  <FormControlLabel disabled control={<Radio />} label="Option 1" />                                        
+              </div>            
+              ): ""}   
+              </ExpansionPanelSummary>
+
+
+              <ExpansionPanelDetails>
+
+              <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginLeft: '15px', marginTop:'-15px'}}>
+                
+                <Typography variant="subtitle1">Form description {i+1}</Typography>
+                
+                <FormControlLabel disabled control={<Radio />} label={<TextField defaultValue="Default Value" />} />     
+                
+                <FormControlLabel disabled control={<Radio />} label="Option 2" />     
+
+                  <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                    sit amet blandit leo lobortis eget.
+                  </Typography>
+              </div>
+              
+              </ExpansionPanelDetails>
+            </ExpansionPanel>
+          </div>
+    </div>
+     )
+    )
+  }
+
+
   return (
        
-          <Paper elevation={2}>
-              <br></br>
-              <br></br>
-
-              <p>Add Title and Description</p>
-              <br></br>
-              <br></br>
-
-          </Paper>
+          <div>
+            {questionsUI()}
+            <div>
+              <button onClick={addMoreQuestionField}>Add question</button>
+            </div>
+            
+          </div>
        
   );
 }
--- src/Components/Form/QuestionsTab.js
+++ src/Components/Form/QuestionsTab.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import QuestionHeader from './QuestionHeader';
 import {Grid} from '@material-ui/core';
+import { Paper, Typography } from '@material-ui/core';
 
 function QuestionsTab() {
+
+  const [formTitle, setFormTitle] = React.useState("");
+  const [formDescription, setFormDescription] = React.useState("");
+
+
+
   return (
-       <div style={{marginTop:'20px'}}>
+       <div style={{marginTop:'15px', marginBottom: '7px'}}>
            <Grid
             container
             direction="column"
@@ -12,10 +19,27 @@ function QuestionsTab() {
             alignItems="center"
             >
               
-             <Grid  xs={12} sm={5}>
-                 <QuestionHeader />
-                 <p>Create or edit qurstions from here Create or edit qurstions from here  Create or edit qurstions from here Create or edit qurstions from here Create or edit qurstions from here Create or edit qurstions from here Create or edit qurstions from here Create or edit qurstions from here</p>
-        
+             <Grid item xs={12} sm={5} style={{width: '100%'}}>
+                 
+                  <Grid style={{borderTop: '10px solid teal', borderRadius: 10}}>
+                      <div>
+                          <div>
+                            <Paper elevation={2} style={{width:'100%'}}>
+                              <div style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginLeft: '15px', paddingTop: '20px', paddingBottom: '20px'}}>
+                              <Typography variant="h4" style={{fontFamily:'sans-serif Roboto', marginBottom:"15px"}}>Good form</Typography>
+                              <Typography variant="subtitle1">Form description</Typography>
+                              </div>
+                            </Paper>
+                          </div> 
+                      </div>       
+                  </Grid>  
+
+                  <Grid style={{paddingTop: '10px'}}>
+                    <div>
+                      <QuestionHeader />
+                    </div>
+                  </Grid>  
+                      
             </Grid>           
            </Grid>
        </div>
