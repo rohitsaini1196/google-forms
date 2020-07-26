12dd0465bab96aca9018c15b89f0b7469f7f7dcb
Rohit Saini <sidsaini1196@gmail.com>
2020-07-26 18:20:19 +0530

    form backend CURD added

--- server/routes/FormRouter.js
+++ server/routes/FormRouter.js
@@ -1,7 +1,10 @@
 var router = require('express').Router()
-const {createForm, formsGet} = require('../services/FormService')
+const {createForm, formsGet, getFormById, deleteForm, editForm} = require('../services/FormService')
 
 router.route("/create").post(createForm)
 router.route("/forms").get(formsGet)
+router.route("/form/:formId").get(getFormById)
+router.route("/deleteform/:formId/:userId").delete(deleteForm)
+router.route("/editform").put(editForm)
 
 module.exports = router;
--- server/services/FormService.js
+++ server/services/FormService.js
@@ -1,4 +1,5 @@
 const FormModel = require('../db/Form')
+const UserModel = require('../db/User')
 const jwt = require('jsonwebtoken');
 const jwtDecode = require('jwt-decode');
 
@@ -22,34 +23,98 @@ module.exports = {
                 description: req.body.description
              }
 
-             
+            var newForm = new FormModel(data)
+            await newForm.save().then((docs)=>{
+                UserModel.updateOne(
+                    {_id: data.createdBy },
+                    { $push: { createdForms: docs._id}})
+                    .then(()=>{
+                    console.log("Form id added to user deeatils");
+                }).catch(error => console.log("got some error"))  
+                res.status(200).json(
+                    docs
+                );
+            })
 
+        } catch (error) {
+            res.send(error)
+        }
+    },
 
+    getFormById: async(req, res)=>{
+        try {
+            var formId = req.params.formId;
+
+            await FormModel.findOne({_id: formId}).then(async(form)=>{
+                 
+                 if(form == null){
+                     res.status(404).send('Form not found');
+                 } else{ 
+                     res.status(200).json(form)
+                 }
+             })
+
+        } catch (error) {
+            res.send(error)
+        }
+    },
+    
+    deleteForm: async(req, res)=>{
+
+        try {
+            var formId = req.params.formId;
+            var userId = req.params.userId;
+
+            console.log(formId);
+            console.log(userId);
+
+            await FormModel.findOne({_id: formId}).then(async(form)=>{ 
+                 console.log(form);
+                if(form== null){
+                    res.status(404).send('Form not found or already deleted');
+                } else { 
+                    if(form.createdBy == userId){
+                        form.remove(function(err) {
+                            if(err) { return res.status(500).send(err) }
+                            console.log('Form deleted');                 
+                            return res.status(202).send("Form Deleted")
+                          });                       
+                    } 
+                    else{
+                        res.status(401).send("You are not the owner of this Form");
+                    }
+                }
+            });
+        } catch (error) {
+            
+        }
+    },
+
+    editForm : async(req, res)=>{
+        try {
+            console.log("afgfdswdefdgfsa");
+            //console.log(req.body);
+            
+            
+
+            var  formId =  req.body.formId;
+            var data = {
+                name: req.body.name,
+                description: req.body.description,
+                questions: req.body.questions
+            }
+
+            FormModel.findByIdAndUpdate(formId, data ,{new: true} ,(err, result)=>{
+                if(err){
+                    res.status(500).send(err)
+                }
+                else{
+                    res.status(202).send("Profile updated")
+                }
+            });
            
         } catch (error) {
             res.send(error)
         }
     }
-
 }
-
-// createdBy: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: 'User'
-//   },
-
-//   name: String,
-
-//   description: {
-//     type: String,
-//     default: ""
-//   },
-
-//   questions : [{
-//     questionText: String,
-//     questionImage: {type: String, default: ""},
-//     options: [{
-//       optionText : String,
-//       optionImage: {type: String, default: ""},
-//     }] 
-//   }],
