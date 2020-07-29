35882722cb50e521f0610c4a9cfe0595042c80be
Rohit Saini <sidsaini1196@gmail.com>
2020-07-29 05:39:33 +0530

    added User view UI (responding form)

---
+++ src/Components/Responding/Footer-trash.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
+
+export default function StickyFooter() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+        
+      <footer className={classes.footer}>
+        <Container maxWidth="sm">
+          <Typography variant="body1">My sticky footer can be found here.</Typography>
+          <Copyright />
+        </Container>
+      </footer>
+    </div>
+  );
+}
--- src/Components/Responding/UserView.js
+++ src/Components/Responding/UserView.js
@@ -8,11 +8,35 @@ import formService from '../../services/formService';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Radio from '@material-ui/core/Radio';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import Divider from '@material-ui/core/Divider';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+ 
+}));
 
 
 
 function UserView(props) {
+  const classes = useStyles();
+
     const [formData, setFormData] = React.useState({});
+    // const [optionResponse, setOptionResponse] = React.useState(1);
+    // console.log(optionResponse);
+    const [value, setValue] = React.useState('female');
+
+    const handleChange = (event) => {
+      setValue(event.target.value);
+    };
+    
+    const [questions, setQuestions] = React.useState([]);
 
     React.useEffect(() => {
         var formId = props.match.params.formId
@@ -22,7 +46,8 @@ function UserView(props) {
         .then((data) => { 
             console.log(data);
             
-            setFormData(data)       
+            setFormData(data)      
+            setQuestions(data.questions) 
            },
            error => {
            const resMessage =
@@ -36,17 +61,32 @@ function UserView(props) {
        );
         
     },[props.match.params.formId]);
+
+    // const handleChange = (event) => {
+    //   console.log(event.target.value);
+      
+    //   setOptionResponse(event.target.value);
+    // };
     return (
-        <div style={{backgroundColor: 'lightblue', height: '100vh'}}>
-             <div style={{marginTop:'15px', marginBottom: '7px', paddingBottom:"30px"}}>
-           <Grid
-            container
-            direction="column"
-            justify="center"
-            alignItems="center"
-            >
-              {/* {loadingFormData ? (<CircularProgress />):""} */}
-              
+        <div style={{minHeight: '100vh'}}>
+          <AppBar position="static" style={{backgroundColor: 'teal'}}>
+            <Toolbar>
+              <IconButton edge="start" style={{marginRight: '10px', marginBottom: '5px'}} color="inherit" aria-label="menu">
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" style={{}}>
+                Velocity Forms
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <br></br>
+
+              <Grid
+                container
+                direction="column"
+                justify="center"
+                alignItems="center"
+              >
                 <Grid item xs={12} sm={5} style={{width: '100%'}}>         
                   <Grid style={{borderTop: '10px solid teal', borderRadius: 10}}>
                         <div>
@@ -61,44 +101,73 @@ function UserView(props) {
                             </Paper>
                           </div> 
                       </div>       
-                  </Grid>     
+                  </Grid>  
 
                   <Grid>
-                  <Paper style={{display: 'flex',flexDirection:'column', alignItems:'flex-start', marginLeft: '3px', paddingTop: '15px', paddingBottom: '15px'}}>
-                {/* <TextField id="standard-basic" label=" " value="Question" InputProps={{ disableUnderline: true }} />  */}
-                
-                <Typography variant="subtitle1" style={{marginLeft: '0px'}}>1. Question Text</Typography>
+                    
+                  { questions.map((ques, i)=>(
+                    <div key={i}>
+                      <br></br>
+                    <Paper>
+                      <div>
+                        <div style={{display: 'flex',
+                                   flexDirection:'column', 
+                                   alignItems:'flex-start', 
+                                   marginLeft: '6px', 
+                                   paddingTop: '15px', 
+                                   paddingBottom: '15px'}}>
+                            <Typography variant="subtitle1" style={{marginLeft: '10px'}}>{i+1}. {ques.questionText}</Typography>
+                           
+                            {ques.questionImage !==""?(
+                                <div>
+                                  <img src={ques.questionImage} width="80%" height="auto" /><br></br><br></br>
+                                </div>
+                            ): "" }
+                            
+                              <div>
+                              {ques.options.map((op, j)=>(
+                                <div key={j}>
+                                  <div style={{display: 'flex', marginLeft: '7px'}}>
+                                    <FormControlLabel control={<Radio style={{marginRight: '3px', }} color="primary" />} label={
+                                        <Typography style={{color: '#555555'}}>
+                                         {op.optionText}
+                                        </Typography>
+                                      } />
+                                  </div>
+                                  <div style={{display: 'flex', marginLeft: '10px'}}>
+                                    {op.optionImage !==""?(
+                                      <img src={op.optionImage} width="64%" height="auto" />
+                                    ): "" }
+                                    <Divider />
+                                  </div>
+                                </div>
+                                ))}  
+                              </div>
+                        </div>
+                      </div>  
+                    </Paper>  
+                    </div>
+                  ))}
+                  </Grid>   
 
+                  <Grid>
+                    <br></br>
+                    <div style={{display: 'flex'}}>
+                      <Button variant="contained" color="primary">Submit</Button>
+                    </div>
+                    <br></br>
 
-                {/* {ques.questionImage !==""?(
-                  <div>
-                    <img src={ques.questionImage} width="400px" height="auto" /><br></br><br></br>
-                  </div>
-                ): "" } */}
-                
-                {["", "", "", ""].map((op, j)=>(
+                    <br></br>
+
+                  </Grid>
+
                  
-                 <div key={j}>
-                   <div style={{display: 'flex'}}>
-                    <FormControlLabel disabled control={<Radio style={{marginRight: '3px', }} />} label={
-                        <Typography style={{color: '#555555'}}>
-                          Option Text
-                        </Typography>
-                      } />
-                   </div>
+                </Grid>  
 
-                  {/* <div>
-                    {op.optionImage !==""?(
-                      <img src={op.optionImage} width="160px" height="auto" />
-                    ): "" }
-                  </div> */}
-                 </div>
-                ))}  
-              </Paper>   
-                  </Grid>      
-                </Grid>           
-           </Grid>
-       </div>
+               
+              </Grid>   
+
+               {/* //TODO: Add a footer here */}
         </div>
     )
 }
